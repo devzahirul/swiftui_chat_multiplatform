624c384d0c4724f9e1101d850a321a8520926109
zahirulAIIUB <jislam150@gmail.com>
2025-09-23 14:30:38 +0600

    Initial Commit

---
+++ swiftuiChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftuiChat
+//
+//  Created by lynkto_1 on 9/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftuiChat/swiftuiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiChatApp.swift
+//  swiftuiChat
+//
+//  Created by lynkto_1 on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
